6d2dd5d564741a1304279411f25767e08846f429
ishidaclar <cishidadias@gmail.com>
2026-03-23 23:06:08 -0300

    repository criado

---
+++ src/main/java/com/clara/ex10/repository/TarefaRepository.java
@@ -0,0 +1,7 @@
+package com.clara.ex10.repository;
+
+import com.clara.ex10.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
+}
